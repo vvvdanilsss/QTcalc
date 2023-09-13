7e66c048298e9b58201843e2d2efff3d41e1ccf6
Andorfim <andrey-efimov-2002@mail.ru>
2023-09-14 01:41:12 +0300

    some changes

--- src/calc_with_python/widget.py
+++ src/calc_with_python/widget.py
@@ -11,7 +11,7 @@ from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
 # Important:
 # You need to run the following command to generate the ui_form.py file
 #     pyside6-uic form.ui -o ui_form.py, or
-#     pyside2-uic form.ui -o ui_form.py
+
 from form_ui import Ui_Widget
 from PySide6 import QtWidgets 
 
